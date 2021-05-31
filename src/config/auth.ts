d5280e2944cf7b8327fbd18badd0feaebcb1bfda
Fabiano Silva <fabianosilva.it@outlook.com>
2021-05-31 17:42:32 -0300

    Fix Date appointment and secret authenticate

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
     jwt: {
-      secret: process.env.APP_SECRET,
+      secret: process.env.APP_SECRET || 'SECRET21231231231321231231231231',
       expiresIn: '1d',
     },
   };
